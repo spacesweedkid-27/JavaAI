89e13f46020692011495f4126b8ccae160719150
Henri Heyden <henri@heyden5.de>
2021-09-05 00:45:35 +0200

    work in progress; experimenting but the Neuron Class seems more finished

--- src/bin/Main.java
+++ src/bin/Main.java
@@ -10,36 +10,29 @@ public class Main {
 
                     @Override
                     public float function(float input) {
-                        return input*input;
+                        if (input >= threshold())
+                        return (float) Math.pow(input+bias(),2);
+                        else return 0;
                     }
 
                     @Override
-                    public float[] weights() {
-                        return new float[] {3,0};
-                    }
-
-                },
-
-                new Neuron() {
-
-                    @Override
-                    public float function(float input) {
-                        return input*input;
+                    public float bias() {
+                        return 1;
                     }
 
                     @Override
-                    public float[] weights() {
-                        return new float[0];
+                    public float threshold() {
+                        return 4;
                     }
-
                 }
-
 
         };
 
+        for (int i = 0; i < 10; i+=1){
+        }
 
-        neuron[0].weights()[1] = (float) 0.3;
+        System.out.println(neuron[0].weights()[0]);
 
-        System.out.println(neuron[0].weights()[1]);
+
     }
 }
--- src/lib/Neuron.java
+++ src/lib/Neuron.java
@@ -2,6 +2,8 @@ package lib;
 
 public abstract class Neuron {
     public abstract float function(float input);
-    public abstract float weights()[];
     public float bias(){return 0;}
+    public float threshold(){return 0;}
+    public float[] weights(){return new float[] {};}
+
 }
