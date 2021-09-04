4fc88405b3381540775175a28acda896dc101c4e
Henri Heyden <henri@heyden5.de>
2021-09-05 01:30:14 +0200

    Really proud of this one, achieving progress on the Network class.

--- src/bin/Main.java
+++ src/bin/Main.java
@@ -1,5 +1,6 @@
 package bin;
 
+import lib.Network;
 import lib.Neuron;
 
 public class Main {
@@ -31,13 +32,68 @@ public class Main {
                     }
 
 
+                },
+
+                new Neuron() {
+
+                    @Override
+                    public float function(float input) {
+                        if (input >= threshold())
+                            return (float) Math.pow(input+bias(),2);
+                        else return 0;
+                    }
+
+                    @Override
+                    public float[] weights() {
+                        return new float[3];
+                    }
+
+                    @Override
+                    public float bias() {
+                        return 1;
+                    }
+
+                    @Override
+                    public float threshold() {
+                        return 4;
+                    }
+
+
+                },
+
+                new Neuron() {
+
+                    @Override
+                    public float function(float input) {
+                        if (input >= threshold())
+                            return (float) Math.pow(input+bias(),2);
+                        else return 0;
+                    }
+
+                    @Override
+                    public float[] weights() {
+                        return new float[3];
+                    }
+
+                    @Override
+                    public float bias() {
+                        return 1;
+                    }
+
+                    @Override
+                    public float threshold() {
+                        return 4;
+                    }
+
+
                 }
 
         };
 
 
+        Network network = new Network(neuron,new int[][]{{0,1,2},{2}});
 
-        System.out.println(neuron[0].weights()[0]);
+        network.displayStructure();
 
 
     }
---
+++ src/lib/Network.java
@@ -0,0 +1,44 @@
+package lib;
+
+public class Network {
+    private final Neuron[][] net;
+
+    public Network(Neuron[] neurons, int[][] map){
+
+        // Finding maximal Y bounds for the map
+
+        int Max = 0;
+        for (int i = 0; i < map.length; i++){
+            if (Max < map[i].length) Max = i;
+        }
+
+        System.out.println(Max);
+
+        // Setting the Neuron[][] net to these bounds
+
+        this.net = new Neuron[map.length][map[Max-1].length];
+
+        // Mapping the neurons to the map
+
+        for (int i = 0; i < map.length; i++){
+            for (int j = 0; j < map[i].length; j++){
+                this.net[i][j] = neurons[map[i][j]];
+            }
+        }
+    }
+
+    // Easy function to display a net
+
+    public void displayStructure(){
+        for (int i = 0; i < net.length; i++){
+            for (int j = 0; j < net[i].length; j++){
+                System.out.print(net[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+}
