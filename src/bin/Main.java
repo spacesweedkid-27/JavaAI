ddb86ed78b8854a7a96b1ce1bbf885da8aaea7f0
Henri Heyden <henri@heyden5.de>
2021-09-05 11:16:32 +0200

    Reworking the Neuron class so that it is not abstract

--- src/bin/Main.java
+++ src/bin/Main.java
@@ -7,15 +7,27 @@ public class Main {
     public static void main(String[]args){
 
 
-        Neuron neuron1 = new Neuron() {
-            @Override
-            public float function(float input) {
-                return (float)Math.pow(input+getBias(),2);
-            }
+        Neuron neurons[] = {
+
+                new Neuron() {
+                    @Override
+                    public float function(float input) {
+                        return (float) Math.pow(input + getBias(), 2);
+                    }
+                },
+                new Neuron() {
+                    @Override
+                    public float function(float input) {
+                        return (float) Math.pow(input + getBias(), 2);
+                    }
+                }
+
         };
 
-        neuron1.setBias(1);
-        System.out.println(neuron1.function(1));
+        neurons[0].setBias(1);
+        neurons[1].setBias(2);
+        System.out.println(neurons[0].function(1));
+        System.out.println(neurons[1].function(1));
 
 
     }
